24815d74d9870990b84aba3e1b88792a49fa1918
alejandra-cas <142620356+alejandra-cas@users.noreply.github.com>
2024-10-10 18:43:34 -0500

    added figures, updated descriptive statistics, benefits_defns, added occupation year analysis, added industry regression models

--- notebooks/benefits_defns.py
+++ notebooks/benefits_defns.py
@@ -1,2 +1,11 @@
-benefits = ['CAREER_DEV', 'WLB', 'WELLBEING', 'RECOGNITION', 'FAMILY', 'CULTURE']
+# benefits = ['CAREER_DEV', 'WLB', 'WELLBEING', 'RECOGNITION', 'FAMILY', 'CULTURE']
 benefits2 = ['CAREER_DEV', 'WLB', 'HEALTH_WELLBEING', 'RECOGNITION', 'FAMILY', 'CULTURE']
+benefits3 = ['CAREER_DEV', 'EDU_ASSISTANCE', 'WLB', 'WELLBEING', 'HEALTH_WELLBEING', 'RECOGNITION', 'FAMILY', 'PARENTAL_LEAVE', 'CULTURE']
+benefits3_labels = ['Career Development', 'Education Assistance', 'Work-Life Balance', 'Wellbeing', 'Health and Wellbeing','Recognition', 'Family', 'Parental Leave', 'Culture']
+
+region = 'STATE_NAME'
+industry = 'NAICS_2022_2_NAME'
+education = 'MIN_EDULEVELS_NAME'
+year = 'YEAR'
+occupation = 'SOC_2021_2_NAME'
+experience = 'EXPERIENCE_BUCKET'
