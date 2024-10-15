60cfb564dc675dbfacf25cf9ec8d87a28757f9d7
alejandra-cas <142620356+alejandra-cas@users.noreply.github.com>
2024-10-14 20:35:21 -0500

    updated label_benefits.py

--- label_benefits.py
+++ label_benefits.py
@@ -3,26 +3,31 @@ import re
 import matplotlib.pyplot as plt
 import numpy as np
 
-even_sample = pd.read_parquet('data/salary_sample_body.parquet.gzip')
+input_path = input("Please enter the input parquet file path: ")
+output_path = input("Please enter the output parquet file path: ")
 
+# even_sample = pd.read_parquet('data/salary_sample_body.parquet.gzip')
+even_sample = pd.read_parquet(input_path)
 
-career_dev = "training programs", "education assistance","tuition reimbursement", "tuition assistance", "mentorship", "career growth", "professional development", "leadership development", "career advancement", "leadership training", "growth opportunities", "personal development", "education reimbursement", "opportunities to grow and develop"
+# career_dev = "training programs", "education assistance","tuition reimbursement", "tuition assistance", "mentorship", "career growth", "professional development", "leadership development", "career advancement", "leadership training", "growth opportunities", "personal development", "education reimbursement", "opportunities to grow and develop"
 edu_assistance = "education assistance","tuition reimbursement", "tuition assistance", "education reimbursement"
 wlb = "paid time off", "PTO", "extra vacation", "flexible vacation", "mental health day", "work-life balance", "work/life balance","generous time-off", "generous time off", "paid vacation", "paid holidays", "holiday pay", "vacation days", "company holiday", "sick leave", "vacation time"
+leave = "paid time off", "PTO", "extra vacation", "flexible vacation", "mental health day", "generous time-off", "generous time off", "paid vacation", "paid holidays", "holiday pay", "vacation days", "company holiday", "sick leave", "vacation time"
 wellbeing = "wellness stipend", "mental health support", "gym membership", "wellness program", "well-being stipend", "wellness programs", "mental health benefits", "employee well-being", "mental wellness support", "wellness and global well-being", "wellness stipend"
 health_wellbeing = "wellness stipend", "health benefits", "mental health support", "gym membership", "wellness program", "well-being stipend", "wellness programs", "mental health benefits", "employee well-being", "health care benefits"
-recognition = "employee of the month", "long-term rewards", "recognition program", "peer recognition", "service awards", "employee recognition"
-family = "parental leave", "childcare assistance", "childcare support", "childcare", "family support", "flexible maternity leave", "paternity leave", "maternity leave", "paid family leave", "child care discount", "paid caregiver/parental", "paid parental", "paid new parent leave"
-parental_leave = "parental leave", "family support", "flexible maternity leave", "paternity leave", "maternity leave", "paid family leave", "paid caregiver/parental", "paid parental", "paid new parent leave"
-culture = "diversity and inclusion", "team culture", "creative freedom", "values-driven", "inclusive culture", "diverse team", "inclusive environment", "value diversity"
-keywords_list = [career_dev,wlb,wellbeing, health_wellbeing, recognition, family, parental_leave, culture]
+# recognition = "employee of the month", "long-term rewards", "recognition program", "peer recognition", "service awards", "employee recognition"
+# family = "parental leave", "childcare assistance", "childcare support", "childcare", "family support", "flexible maternity leave", "paternity leave", "maternity leave", "paid family leave", "child care discount", "paid caregiver/parental", "paid parental", "paid new parent leave"
+parental_leave = "parental leave", "family support", "flexible maternity leave", "paternity leave", "maternity leave", "paid family leave", "paid caregiver/parental", "paid parental", "paid new parent leave", "paid bonding leave", "parental bonding leave"
+culture = "diversity and inclusion", "team culture", "creative freedom", "values-driven", "inclusive culture", "diverse team", "inclusive environment", "value diversity", "diversity, equity", "culture of diversity", "diversity, inclusion", "inclusion and diversity", "commitment to diversity", "diversity, inclusion", "diversity, equity and inclusion", "diversity is respected", "inclusive diversity", "workforce diversity", "embracing diversity", "value diversity", "values diversity", "committed to diversity", "equity and diversity", "promoting diversity", "celebrate diversity", "encourage diversity", "support diversity", "diversity in the workplace", "equity, inclusion"
+# keywords_list = [career_dev,wlb,wellbeing, health_wellbeing, recognition, family, parental_leave, culture]
 
 # export keywords list as pickle
-import pickle
-with open('data/keywords_list.pkl', 'wb') as f:
-    pickle.dump(keywords_list, f)
+# import pickle
+# with open('data/keywords_list.pkl', 'wb') as f:
+#     pickle.dump(keywords_list, f)
 
 career_dev_to_exclude = ["youth leadership development", "provide mentorship", "professional development expertise","planning professional development", "forecasting growth opportunities", "teen leadership development", "develop training programs", "professional development experience", "training programs as required", "providing mentorship", "providing and encouraging mentorship", "implementing training programs", "provide leadership and mentorship", "provide mentorship", "identify growth opportunities", "manage the Symbotic Tuition Reimbursement"]
+tuition_to_exclude = ["manage the Symbotic Tuition Reimbursement"]
 wellbeing_to_exclude = ["health benefits companies", "wellness program coordinator", "familiarity with mental health support", "health benefits administration"]
 recognition_to_exclude = ["managing employee recognition"]
 family_to_exclude = ["accredited childcare program", "maui family support services", "teaching assistant - childcare", "experience in childcare", "childcare state licensing", "experience with children"]
@@ -77,17 +82,18 @@ def plot_counts(df, benefit):
     ax.set_xlabel(None)
     ax.set_ylabel('Jobs')
     
-check_benefits(even_sample, career_dev, 'CAREER_DEV', exclusions=career_dev_to_exclude)
-check_benefits(even_sample, edu_assistance, 'EDU_ASSISTANCE', exclusions=career_dev_to_exclude)
+# check_benefits(even_sample, career_dev, 'CAREER_DEV', exclusions=career_dev_to_exclude)
+check_benefits(even_sample, edu_assistance, 'EDU_ASSISTANCE', exclusions=tuition_to_exclude)
 check_benefits(even_sample, wlb, 'WLB')
-check_benefits(even_sample, wellbeing, 'WELLBEING', wellbeing_to_exclude)
+check_benefits(even_sample, leave, 'LEAVE')
+# check_benefits(even_sample, wellbeing, 'WELLBEING', wellbeing_to_exclude)
 check_benefits(even_sample, health_wellbeing, 'HEALTH_WELLBEING', wellbeing_to_exclude)
-check_benefits(even_sample, recognition, 'RECOGNITION', recognition_to_exclude)
-check_benefits(even_sample, family, 'FAMILY', family_to_exclude)
+# check_benefits(even_sample, recognition, 'RECOGNITION', recognition_to_exclude)
+# check_benefits(even_sample, family, 'FAMILY', family_to_exclude)
 check_benefits(even_sample, parental_leave, 'PARENTAL_LEAVE')
-check_benefits(even_sample, culture, 'CULTURE', culture_to_exclude)
+check_benefits(even_sample, culture, 'CULTURE')
 
 print(even_sample.head())
 
-even_sample.to_parquet('data/salary_sample_body_benefits_v3.parquet.gzip', compression='gzip')
+even_sample.to_parquet(output_path, compression='gzip')
 
