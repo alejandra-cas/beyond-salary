e41d996908a1be49878a0a1a5d0e9a9b671016b0
alejandra <alejandra.cm033@gmail.com>
2024-10-18 17:54:31 +0100

    benefits by occupation plots

--- package_files/benefits_defns.py
+++ package_files/benefits_defns.py
@@ -3,6 +3,7 @@ benefits2 = ['CAREER_DEV', 'WLB', 'HEALTH_WELLBEING', 'RECOGNITION', 'FAMILY', '
 benefits3 = ['CAREER_DEV', 'EDU_ASSISTANCE', 'WLB', 'WELLBEING', 'HEALTH_WELLBEING', 'RECOGNITION', 'FAMILY', 'PARENTAL_LEAVE', 'CULTURE']
 benefits3_labels = ['Career Development', 'Education Assistance', 'Work-Life Balance', 'Wellbeing', 'Health and Wellbeing','Recognition', 'Family', 'Parental Leave', 'Culture']
 benefits4 = ['EDU_ASSISTANCE', 'WLB', 'PAID_LEAVE','HEALTH_WELLBEING', 'PARENTAL_LEAVE', 'CULTURE', 'wfh_wham']
+benefits_labels_map = {'EDU_ASSISTANCE': 'Tuition Assistance', 'WLB': 'Work-Life Balance', 'PAID_LEAVE': 'Paid Leave', 'HEALTH_WELLBEING': 'Health and Wellbeing', 'PARENTAL_LEAVE': 'Parental Leave', 'CULTURE': 'Workplace Culture', 'wfh_wham': 'Remote Work'}
 
 region = 'STATE_NAME'
 industry = 'NAICS_2022_2_NAME'
