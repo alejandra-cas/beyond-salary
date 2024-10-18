366767b4ecdc17a4dd38b5bcd83e738c24c5ae44
alejandra-cas <142620356+alejandra-cas@users.noreply.github.com>
2024-10-18 10:49:49 -0500

    end oct 17

--- package_files/benefits_defns.py
+++ package_files/benefits_defns.py
@@ -3,6 +3,7 @@ benefits2 = ['CAREER_DEV', 'WLB', 'HEALTH_WELLBEING', 'RECOGNITION', 'FAMILY', '
 benefits3 = ['CAREER_DEV', 'EDU_ASSISTANCE', 'WLB', 'WELLBEING', 'HEALTH_WELLBEING', 'RECOGNITION', 'FAMILY', 'PARENTAL_LEAVE', 'CULTURE']
 benefits3_labels = ['Career Development', 'Education Assistance', 'Work-Life Balance', 'Wellbeing', 'Health and Wellbeing','Recognition', 'Family', 'Parental Leave', 'Culture']
 benefits4 = ['EDU_ASSISTANCE', 'WLB', 'PAID_LEAVE','HEALTH_WELLBEING', 'PARENTAL_LEAVE', 'CULTURE', 'wfh_wham']
+benefits4_labels = ['Tuition Assistance', 'Work-Life Balance', 'Paid Leave', 'Health and Wellbeing', 'Parental Leave', 'Workplace Culture', 'Remote Work']
 
 region = 'STATE_NAME'
 industry = 'NAICS_2022_2_NAME'
